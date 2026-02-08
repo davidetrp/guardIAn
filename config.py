3e127f4246b487840897c571547703c1eaf206f7
davidetrp <davidetrupia01@gmail.com>
2026-02-08 12:27:24 +0100

    Create config.py
    Configuration file to be changed in case of differences to adapt. USED IN RASPBERRY PI ZERO2W

---
+++ config.py
@@ -0,0 +1,12 @@
+# Zona di interesse (percentuale del frame)
+ROI_Y_START = 0.5     # metà inferiore: soglia/porta
+ROI_Y_END   = 1.0
+
+# Motion detection
+MIN_AREA = 5000        # area minima (da tarare)
+FRAMES_REQUIRED = 3    # frame consecutivi
+COOLDOWN_SEC = 20      # evita raffiche
+
+# Camera
+LOW_RES = (320, 240)
+HIGH_RES = (1280, 720)
